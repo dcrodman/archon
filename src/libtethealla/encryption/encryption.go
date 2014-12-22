648a9d7268f16a2cd07fd6b101fc1c898c581c77
Drew Rodman <dcrodman@gmail.com>
2014-12-22 18:13:39 -0500

    Implement wrapper struct for CRYPT_SETUP in encryption lib.
    Makes it possible to pass the crypt struct around without having to
    include "C" and encryption.h in every source file that references it.

--- src/libtethealla/encryption/encryption.go
+++ src/libtethealla/encryption/encryption.go
@@ -4,25 +4,43 @@ package encryption
 import "C"
 
 /* Wrapper library for Fuzzier's encryption lib. */
-import "unsafe"
 
-// Returns a newly allocated C CRYPT_SETUP
-func NewCrypt() *C.CRYPT_SETUP {
-	return new(C.CRYPT_SETUP)
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"unsafe"
+)
+
+type PSOCrypt struct {
+	cryptSetup C.CRYPT_SETUP
+	Vector     []uint8
 }
 
 // Initializes a CRYPT_SETUP with a 48-byte key.
-func CreateKeys(crypt_setup *C.CRYPT_SETUP, keys []byte) int {
-	return int(C.CRYPT_CreateKeys(crypt_setup, unsafe.Pointer(&keys[0]), C.CRYPT_BLUEBURST))
+func (crypt *PSOCrypt) CreateKeys() int {
+	crypt.Vector = make([]uint8, 48)
+	for i := 0; i < 48; i++ {
+		binary.Read(rand.Reader, binary.LittleEndian, &(crypt.Vector[i]))
+	}
+	return int(C.CRYPT_CreateKeys(&crypt.cryptSetup, unsafe.Pointer(&crypt.Vector[0]), C.CRYPT_BLUEBURST))
+}
+
+// Convenience wrapper for CryptData with encrypting = 1.
+func (crypt *PSOCrypt) Encrypt(data []byte, size uint32) {
+	CryptData(crypt, data, size, C.int(1))
+}
+
+// Convenience wrapper for CryptData with encrypting = 0.
+func (crypt *PSOCrypt) Decrypt(data []byte, size uint32) {
+	CryptData(crypt, data, size, C.int(0))
+}
+
+// Returns a newly allocated and zeroed PSOCrypt.
+func NewCrypt() *PSOCrypt {
+	return new(PSOCrypt)
 }
 
 // Encrypt or decrypt the packet pointed to by data in-place using crypt_setup.
-func CryptData(crypt_setup *C.CRYPT_SETUP, data []byte, size uint32, encrypting bool) int {
-	var encr C.int
-	if encrypting {
-		encr = 1
-	} else {
-		encr = 0
-	}
-	return int(C.CRYPT_CryptData(crypt_setup, unsafe.Pointer(&data[0]), C.ulong(size), encr))
+func CryptData(crypt *PSOCrypt, data []byte, size uint32, encrypting C.int) int {
+	return int(C.CRYPT_CryptData(&crypt.cryptSetup, unsafe.Pointer(&data[0]), C.ulong(size), encrypting))
 }
