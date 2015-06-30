5fd6a725c18d812307943e03b2c5f753d0bf3fcf
Drew Rodman <dcrodman@gmail.com>
2015-06-29 23:24:46 -0700

    Remove develop dir and add tools.
    This should make it clearer that the tools directory includes
    things that can actually be used for the server. The contents
    of the develop directory can always be manually shared with others.

--- develop/testdb.sql
+++
@@ -1,7 +0,0 @@
-grant all on archondb.* to 'archonadmin'@'localhost' identified by 'psoadminpassword';
-
-insert into account_data (username, password, email, registration_date, is_gm, is_active, team_id) 
-values ('test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'test@email.com', now(), true, true, 1);
-
-insert into account_data (username, password, email, registration_date, is_gm, is_active, team_id) 
-values ('rabble', '5f121158d2145c5d68a82ba2d2a8052e1fffc7ed14ae601befc91e339e387860', 'rabble@email.com', now(), false, true, 2);
