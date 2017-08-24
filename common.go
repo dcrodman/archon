6580ca315e462c9a082e92bf75679c4cf9658e55
Drew Rodman <dcrodman@gmail.com>
2017-08-23 20:03:48 -0700

    Move character structs into common and defaults into own file

--- common.go
+++ common.go
@@ -25,6 +25,88 @@ import (
 	"github.com/dcrodman/archon/util"
 )
 
+// CharClass is an enumeration of the possible character classes.
+type CharClass uint8
+
+const (
+	// Possible character classes as defined by the game.
+	Humar     CharClass = 0x00
+	Hunewearl           = 0x01
+	Hucast              = 0x02
+	Ramar               = 0x03
+	Racast              = 0x04
+	Racaseal            = 0x05
+	Fomarl              = 0x06
+	Fonewm              = 0x07
+	Fonewearl           = 0x08
+	Hucaseal            = 0x09
+	Fomar               = 0x0A
+	Ramarl              = 0x0B
+)
+
+// Per-player friend guildcard entries.
+type GuildcardEntry struct {
+	Guildcard   uint32
+	Name        [24]uint16
+	TeamName    [16]uint16
+	Description [88]uint16
+	Reserved    uint8
+	Language    uint8
+	SectionID   uint8
+	CharClass   uint8
+	padding     uint32
+	Comment     [88]uint16
+}
+
+// Per-player guildcard data chunk.
+type GuildcardData struct {
+	Unknown  [0x114]uint8
+	Blocked  [0x1DE8]uint8 //This should be a struct once implemented
+	Unknown2 [0x78]uint8
+	Entries  [104]GuildcardEntry
+	Unknown3 [0x1BC]uint8
+}
+
+// Struct used by Character Info packet.
+type CharacterPreview struct {
+	Experience     uint32
+	Level          uint32
+	GuildcardStr   [16]byte
+	Unknown        [2]uint32
+	NameColor      uint32
+	Model          byte
+	Padding        [15]byte
+	NameColorChksm uint32
+	SectionId      byte
+	Class          byte
+	V2flags        byte
+	Version        byte
+	V1Flags        uint32
+	Costume        uint16
+	Skin           uint16
+	Face           uint16
+	Head           uint16
+	Hair           uint16
+	HairRed        uint16
+	HairGreen      uint16
+	HairBlue       uint16
+	PropX          float32
+	PropY          float32
+	Name           [24]uint8
+	Playtime       uint32
+}
+
+// Per-character stats.
+type CharacterStats struct {
+	ATP uint16
+	MST uint16
+	EVP uint16
+	HP  uint16
+	DFP uint16
+	ATA uint16
+	LCK uint16
+}
+
 // VerifyAccount performs all account verification tasks.
 func VerifyAccount(client *Client) (*LoginPkt, error) {
 	var loginPkt LoginPkt
