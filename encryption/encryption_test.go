7288540bf9d4aa6e44cae01b26911439066b5688
Drew Rodman <dcrodman@gmail.com>
2020-04-21 19:40:57 -0700

    Remove leftover code from encryption_test

--- encryption/encryption_test.go
+++ encryption/encryption_test.go
@@ -5,47 +5,6 @@ import (
 	"testing"
 )
 
-//func TestPSOCrypt(t *testing.T) {
-//	tests := []struct {
-//		name    string
-//		cryptFn func() *PSOCrypt
-//	}{
-//		{"BB Crypt", NewBBCrypt},
-//		{"PC Crypt", NewPCCrypt},
-//	}
-//
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			crypt := tt.cryptFn()
-//			testData := []byte("test data with padding _")
-//
-//			encBuffer := make([]byte, len(testData))
-//			copy(encBuffer, testData)
-//			crypt.Encrypt(encBuffer, uint32(len(encBuffer)))
-//
-//			if reflect.DeepEqual(encBuffer, testData) {
-//				t.Fatalf("expected Encrypt() to have encrypted data")
-//			}
-//
-//			decBuffer := make([]byte, len(testData))
-//			copy(decBuffer, encBuffer)
-//			crypt.Decrypt(decBuffer, uint32(len(decBuffer)))
-//
-//			if !reflect.DeepEqual(decBuffer, testData) {
-//				t.Fatalf("expected Decrypt() to have decrypted to the original string")
-//			}
-//
-//			buffer2 := make([]byte, len(testData))
-//			copy(buffer2, testData)
-//			tt.cryptFn().Encrypt(buffer2, uint32(len(buffer2)))
-//
-//			if reflect.DeepEqual(buffer2, encBuffer) {
-//				t.Fatalf("expected new cipher to have used a different vector")
-//			}
-//		})
-//	}
-//}
-
 func TestPSOCrypt_BB(t *testing.T) {
 	crypt := NewBBCrypt()
 	testData := []byte("test data with padding _")
