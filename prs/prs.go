1b332af84265ae34bf31e4c6d4c683bb4647597a
Drew Rodman <dcrodman@gmail.com>
2020-05-13 20:23:27 -0700

    Rename entrypoint files in cmd/ to main.go

--- prs/prs.go
+++ prs/prs.go
@@ -4,27 +4,8 @@ package prs
 import "C"
 import "unsafe"
 
-/*
-* Archon PSO Server
-* Copyright (C) 2014 Andrew Rodman
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-* ---------------------------------------------------------------------
-*
-* PRS compression/decompression library. Original underlying C implementation
-* by Fuzziqer Software, wrapper written in Go for use with archon.
- */
+// PRS compression/decompression library. Original underlying C implementation
+// by Fuzziqer Software, wrapper written in Go for use with archon.
 
 func Compress(src, dest []byte, size uint32) uint32 {
 	return uint32(C.prs_compress(
