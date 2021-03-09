c897034b84dcf8ca11b0b2d2420e63a74c7f77e0
Drew Rodman <dcrodman@gmail.com>
2021-03-08 22:17:52 -0800

    Fix ship server copyright

--- internal/server/ship/ship.go
+++ internal/server/ship/ship.go
@@ -22,7 +22,7 @@ import (
 // BackMenuItem is the block ID reserved for returning to the ship select menu.
 const BackMenuItem = 0xFF
 
-var loginCopyright = []byte("Phantasy Star Online Blue Burst Game Backend. Copyright 1999-2004 SONICTEAM.")
+var loginCopyright = []byte("Phantasy Star Online Blue Burst Game Server. Copyright 1999-2004 SONICTEAM.")
 
 type Server struct {
 	name string
