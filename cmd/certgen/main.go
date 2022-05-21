063d2652b26bf4684de22da93114df01e6f6f66e
Drew Rodman <dcrodman@gmail.com>
2022-05-21 00:10:13 -0400

    improvements to cert gen tool

--- cmd/certgen/main.go
+++ cmd/certgen/main.go
@@ -25,6 +25,7 @@ import (
 	"math/big"
 	"net"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -33,27 +34,35 @@ const (
 	privateKeyFilename  = "key.pem"
 )
 
-var ip = flag.String("ip", "", "Server's external_ip (in config.yaml) or CIDR block")
+var ip = flag.String("ip", "", "Server's external_ip or comma-separated IPs")
 
 func main() {
 	flag.Parse()
-	serverIP := *ip
-	if serverIP == "" {
-		fmt.Print("server's external_ip (in config.yaml) or CIDR block: ")
+	serverIP := make([]string, 0)
+	if *ip == "" {
+		// Read in a list of IPs.
 		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		serverIP = scanner.Text()
+		for {
+			fmt.Print("server's external_ip: ")
+			scanner.Scan()
+			if scanner.Text() == "" {
+				break
+			}
+			serverIP = append(serverIP, scanner.Text())
+		}
+	} else {
+		serverIP = strings.Split(*ip, ",")
 	}
 
 	template, err := createX509Template(serverIP)
 	if err != nil {
-		fmt.Println("failed to create X.509 template:", err)
+		fmt.Println("error creating X.509 template:", err)
 		return
 	}
 
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		fmt.Printf("Error generating RSA key: %s\n", err.Error())
+		fmt.Printf("error generating RSA key: %s\n", err.Error())
 		return
 	}
 
@@ -71,19 +80,20 @@ func main() {
 	)
 }
 
-func createX509Template(serverIP string) (*x509.Certificate, error) {
+func createX509Template(serverIPs []string) (*x509.Certificate, error) {
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
 		return nil, err
 	}
 
-	ip, ipnet, err := net.ParseCIDR(serverIP)
-	if err != nil {
-		ip = net.ParseIP(serverIP)
-		if ip == nil {
-			return nil, fmt.Errorf("%v is not a valid IP address or CIDR block", serverIP)
+	var ips []net.IP
+	for _, ip := range serverIPs {
+		parsedIP := net.ParseIP(ip)
+		if parsedIP == nil {
+			return nil, fmt.Errorf("%v is not a valid IP address or CIDR block", ip)
 		}
+		ips = append(ips, parsedIP)
 	}
 
 	notBefore := time.Now()
@@ -101,10 +111,7 @@ func createX509Template(serverIP string) (*x509.Certificate, error) {
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 		BasicConstraintsValid: true,
 		IsCA:                  true,
-		IPAddresses:           []net.IP{ip},
-	}
-	if ipnet != nil {
-		template.PermittedIPRanges = []*net.IPNet{ipnet}
+		IPAddresses:           ips,
 	}
 	return template, nil
 }
@@ -118,13 +125,13 @@ func generateCertificateFile(template *x509.Certificate, privateKey *rsa.Private
 
 	certOut, err := os.Create("certificate.pem")
 	if err != nil {
-		fmt.Printf("failed to create %s: %s\n", certificateFilename, err)
+		fmt.Printf("error creating certificate %s: %s\n", certificateFilename, err)
 		return
 	}
 
 	err = pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes})
 	if err != nil {
-		fmt.Printf("failed to create %s: %s", certificateFilename, err)
+		fmt.Printf("error encoding certificate %s: %s", certificateFilename, err)
 		return
 	}
 	certOut.Close()
@@ -135,14 +142,14 @@ func generateCertificateFile(template *x509.Certificate, privateKey *rsa.Private
 func generatePrivateKeyFile(privateKey *rsa.PrivateKey) {
 	keyOut, err := os.OpenFile("key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Printf("failed to create %s: %s\n", privateKeyFilename, err)
+		log.Printf("error creating key %s: %s\n", privateKeyFilename, err)
 		return
 	}
 
 	keyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
 	err = pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: keyBytes})
 	if err != nil {
-		fmt.Printf("failed to create %s: %s\n", privateKeyFilename, err)
+		fmt.Printf("error encoding key %s: %s\n", privateKeyFilename, err)
 		return
 	}
 	keyOut.Close()
